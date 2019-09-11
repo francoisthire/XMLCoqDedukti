96db162cb58b3b09b06f179a557c942174e2b0db
Claudio Sacerdoti Coen <claudio.sacerdoticoen@unibo.it>
2019-09-11 16:22:38 +0200

    disk mapping

--- src/main.ml
+++ src/main.ml
@@ -1,9 +1,21 @@
 include CicParser;;
 
-let obj =
- CicParser.obj_of_xml
-  (UriManager.uri_of_string "cic:/Coq/Init/Nat/add.con")
-  "test/add.con.xml.gz"
-  (Some "test/add.con.body.xml.gz") in
-let d = CicToDedukti.dedukti_of_obj obj in
-Format.printf "%a\n" Dkprint.pp_entry d
+let basepathname uri =
+ let buri = UriManager.buri_of_uri uri in
+ "test/" ^ String.sub buri 5 (String.length buri - 5)
+
+let conpathname uri =
+ basepathname uri ^ "/" ^ UriManager.name_of_uri uri ^ ".con.xml.gz"
+
+let conbodypathname uri =
+ basepathname uri ^ "/" ^ UriManager.name_of_uri uri ^ ".con.body.xml.gz"
+ 
+
+let do_obj uri =
+ let obj =
+  CicParser.obj_of_xml uri
+   (conpathname uri) (Some (conbodypathname uri)) in
+ let d = CicToDedukti.dedukti_of_obj obj in
+ Format.printf "%a\n" Dkprint.pp_entry d
+
+let _ = do_obj (UriManager.uri_of_string "cic:/Coq/Init/Nat/add.con")
